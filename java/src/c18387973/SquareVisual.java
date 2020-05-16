e4d14513f95120fc0827e4f4aaf6813182851508
erife <erifeoluwa@gmail.com>
2020-05-16 05:41:52 +0100

    finished the square visual

--- java/src/c18387973/SquareVisual.java
+++ java/src/c18387973/SquareVisual.java
@@ -17,9 +17,7 @@ public class SquareVisual extends MyVision  {
     public void render()
     {
         mv.colorMode(PApplet.HSB);
-        mv.strokeWeight(25);
-        //mv.stroke(PApplet.map(i, 0, mv.getAudioBuffer().size(), 0, 255), 255, 255);
-        //mv.line(i, cy, i + i  , cy + cy * mv.getAudioBuffer().get(i));
+        mv.strokeWeight(15);
         mv.translate(mv.width/2, mv.height/2);
         float scaleVar = (PApplet.map(10, 0, mv.width, 5,5));
         mv.scale(scaleVar);
@@ -31,8 +29,27 @@ public class SquareVisual extends MyVision  {
                 mv.fill(0);
                 mv.scale(0.95F);
                 mv.rotate(angle);
+
+                //bottom of screen
                 mv.rect(400,600, 500,300*mv.getAudioBuffer().get(i));
-                mv.rect(100,100, 300,300*mv.getAudioBuffer().get(i));
+                mv.rect(-400,600, 500,300*mv.getAudioBuffer().get(i));
+
+                //right corner
+                mv.rect(1000,-600, 300,300*mv.getAudioBuffer().get(i));
+                //mv.rect(400,-600, 500,300*mv.getAudioBuffer().get(i));
+                mv.rect(1000,-800, 300,300*mv.getAudioBuffer().get(i));
+
+                //
+                mv.rect(-10,-600, 300,300*mv.getAudioBuffer().get(i));
+                mv.rect(-10,-900, 300,300*mv.getAudioBuffer().get(i));
+
+                mv.rect(10,-200, 800,300*mv.getAudioBuffer().get(i));
+
+                //left corner
+                mv.stroke(PApplet.map(i, 0, mv.getAudioBuffer().size(), 0, 255*mv.getAudioBuffer().get(i)),  150, 200);
+                mv.fill(0);
+                mv.rect(-400,-100, 500,300*mv.getAudioBuffer().get(i));
+
             }
         }
         angle +=0.1;
